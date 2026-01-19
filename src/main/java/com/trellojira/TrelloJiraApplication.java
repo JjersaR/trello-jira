eaf2c79e8a42699ace5d36ec04e2264382b1048b
JjersaR <rikisj5649@gmail.com>
2026-01-19 10:33:17 -0600

    first commit

---
+++ src/main/java/com/trellojira/TrelloJiraApplication.java
@@ -0,0 +1,13 @@
+package com.trellojira;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrelloJiraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrelloJiraApplication.class, args);
+	}
+
+}
