293b2a9e5d187569d23519286e78a2eb66b87b72
JjersaR <rikisj5649@gmail.com>
2026-01-22 10:59:18 -0600

    Functional Card

--- src/main/java/com/trellojira/card/controller/MutationController.java
+++ src/main/java/com/trellojira/card/controller/MutationController.java
@@ -1,16 +1,29 @@
 package com.trellojira.card.controller;
 
+import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.MutationMapping;
 import org.springframework.stereotype.Controller;
 
 import com.trellojira.card.service.CardService;
-import com.trellojira.dto.mapper.ModelMapper;
+import com.trellojira.dto.request.CardRequest;
 
 import lombok.RequiredArgsConstructor;
 
+@Controller
 @RequiredArgsConstructor
 public class MutationController {
 
   private final CardService service;
-  private final ModelMapper mapper;
 
+  @MutationMapping(name = "createCard")
+  public String save(@Argument CardRequest card) {
+    service.save(card);
+    return "Tarjeta creada";
+  }
+
+  @MutationMapping(name = "deleteCard")
+  public String delete(@Argument Long id) {
+    service.delete(id);
+    return "Tarjeta borrada";
+  }
 }
--- src/main/java/com/trellojira/card/controller/QueryController.java
+++ src/main/java/com/trellojira/card/controller/QueryController.java
@@ -1,16 +1,35 @@
 package com.trellojira.card.controller;
 
+import java.util.List;
+
+import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.stereotype.Controller;
 
 import com.trellojira.card.service.CardService;
-import com.trellojira.dto.mapper.ModelMapper;
+import com.trellojira.dto.response.CardResponse;
 
 import lombok.RequiredArgsConstructor;
 
+@Controller
 @RequiredArgsConstructor
 public class QueryController {
 
   private final CardService service;
-  private final ModelMapper mapper;
+
+  @QueryMapping(name = "findByColumn")
+  public List<CardResponse> getByColumnId(@Argument Integer id) {
+    return service.getByColumnId(Long.parseLong(id.toString()));
+  }
+
+  @QueryMapping(name = "findByAssignedTo")
+  public List<CardResponse> getByAssignedToId(@Argument Integer id) {
+    return service.getByAssignedToId(Long.parseLong(id.toString()));
+  }
+
+  @QueryMapping(name = "findByColumnOrder")
+  public List<CardResponse> getByColumnIdOrder(@Argument Integer id) {
+    return service.getByColumnIdOrder(Long.parseLong(id.toString()));
+  }
 
 }
